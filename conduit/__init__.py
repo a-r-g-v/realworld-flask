b82d0bcabb6d1663028feade9d2bb6ded17b1a95
greyia <info@thun.ws>
2017-05-08 00:58:03 +0900

    set strict_slashes is false

--- conduit/__init__.py
+++ conduit/__init__.py
@@ -3,16 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 from .errors import Errors
 
 migrate = Migrate()
 jwt = JWTManager()
 errors = Errors()
 ma = Marshmallow()
+cors = CORS()
 
 
 def create_app(config_filename=None, config_object=None):
     app = Flask(__name__)
+    app.url_map.strict_slashes = False
 
     if config_filename:
         app.config.from_pyfile(config_filename)
@@ -27,6 +30,8 @@ def create_app(config_filename=None, config_object=None):
     migrate.init_app(app, db)
 
     jwt.init_app(app)
+
+    cors.init_app(app, resources={r"/api/*": {"origins": "*"}})
 
     from .views import api as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
